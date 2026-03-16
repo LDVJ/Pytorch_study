1cdf74437228d43888817098c429d794d9356a97
Jyotirmay Verma <vermajyotirmay54@gmail.com>
2026-03-16 20:26:45 +0530

    creation of different dimmension of tensor -scalar - 0D -vector - 1D -matrix - 2D -tensor -  nD

--- app/main.py
+++ app/main.py
@@ -1,4 +1,30 @@
 import torch
 
-x = torch.rand(5, 3)
-print(x)
+scalar = torch.tensor(7)
+
+vector = torch.tensor([1,2])
+
+MATRIX = torch.tensor([[1,2],[3,4]])
+
+TENSOR = torch.tensor([[[1,2,3],[4,5,6],[7,8,9]],[[9,8,7],[6,5,4],[3,2,1]]])
+
+# basic scalr based functions
+print(scalar)
+print(scalar.item())
+print(scalar.ndim)
+print(scalar.shape)
+
+# vector basic functions
+print(vector)
+print(vector.ndim)
+print(vector.shape)
+
+# MATRIX basic functions
+print(MATRIX)
+print(MATRIX.ndim)
+print(MATRIX.shape)
+
+#TENSOR basic functions
+print(TENSOR)
+print(TENSOR.ndim)
+print(TENSOR.shape)
