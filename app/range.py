7b7e5ab995f25e9b546b3344764faa4b36f7620f
Jyotirmay Verma <vermajyotirmay54@gmail.com>
2026-03-18 02:06:43 +0530

    rand-like exploring

--- app/range.py
+++ app/range.py
@@ -1,6 +1,17 @@
 import torch
 
-range_tensor = torch.arange(start=10, end=0, step= -2, dtype=torch.int32)
+# range_tensor = torch.arange(start=10, end=0, step= -2)
 
+normal_tensor = torch.rand(size=(3,4), dtype=torch.int32)
 
-print(range_tensor)
+# copy_range = torch.ones_like(range_tensor)
+
+new_tensor = torch.rand_like(normal_tensor, dtype=torch.int32)
+
+# print(range_tensor)
+
+# print(copy_range)
+
+print(normal_tensor)
+
+print(new_tensor)
